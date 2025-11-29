2a42cfa6c8c04e86b775d6c256d6a9ddff120d61
kishore babu <shkishore8@gmail.com>
2025-11-29 06:39:56 +0530

    Roboshop cart

---
+++ dispatch.sh
@@ -0,0 +1,20 @@
+dnf install golang -y
+
+useradd roboshop
+cp dispatch.service /etc/systemd/system/dispatch.service
+
+rm -fr /app
+mkdir /app
+
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
+cd /app
+unzip /tmp/dispatch.zip
+
+cd /app
+go mod init dispatch
+go get
+go build
+
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl start dispatch
